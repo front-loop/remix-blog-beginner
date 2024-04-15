0f9a84acf0464c5d755a51df3e794daecf0569a2
mancuoj <mancuoj@gmail.com>
2024-04-15 23:01:06 +0800

    chore: nextui

--- app/root.tsx
+++ app/root.tsx
@@ -1,8 +1,11 @@
-import { LinksFunction } from '@remix-run/node'
+import { LinksFunction, MetaFunction } from '@remix-run/node'
 import { Links, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react'
 import stylesheet from './tailwind.css?url'
+import { NextUIProvider } from '@nextui-org/react'
 
 export const links: LinksFunction = () => [{ rel: 'stylesheet', href: stylesheet }]
+
+export const meta: MetaFunction = () => [{ title: 'Remix Blog' }]
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -23,5 +26,9 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
-  return <Outlet />
+  return (
+    <NextUIProvider>
+      <Outlet />
+    </NextUIProvider>
+  )
 }
